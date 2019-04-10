1691390ab4ffa48d5e083da8c68158d4780397cd
Alessia Bellisario <alessia@breather.com>
2019-04-09 21:33:58 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -32,16 +32,16 @@ impl Canvas {
     }
 
     pub fn initial_draw(&self) {
-        draw_grid_to_canvas(&self.context, 0, 0, self.range);
+        // draw_grid_to_canvas(&self.context, 0, 0, self.range);
     }
 
     pub fn enable_mouse_tracking(&self) {
         let tracking_enabled = Rc::new(Cell::new(false));
         {
             let range = self.range.clone();
-
             let context = self.context.clone();
             let tracking_enabled = tracking_enabled.clone();
+
             let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
                 if !tracking_enabled.get() {
                     return;
@@ -49,7 +49,7 @@ impl Canvas {
                 context.clear_rect(0.0, 0.0, 2000.0, 2000.0);
                 let x_offset = event.offset_x();
                 let y_offset = event.offset_y();
-                draw_grid_to_canvas(&context, x_offset as u32, y_offset as u32, range)
+                let faces = calculate_faces(&context, x_offset as u32, y_offset as u32, range);
             }) as Box<dyn FnMut(_)>);
             self.canvas
                 .add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())
@@ -94,34 +94,31 @@ impl Canvas {
     }
 }
 
-fn draw_grid_to_canvas(context: &Context2d, skew_x: u32, skew_y: u32, range: u32) {
+fn calculate_faces(context: &Context2d, skew_x: u32, skew_y: u32, range: u32) -> Faces {
     const WIDTH: u32 = 6;
     const HEIGHT: u32 = 6;
     const GLYPH_WIDTH: u32 = 130;
+    let mut arcs = Vec::new();
 
     for a in 0..(WIDTH * HEIGHT) {
         for i in 1..10 {
             let y_offset = a / WIDTH * GLYPH_WIDTH + range;
             let x_offset = (a % WIDTH) * GLYPH_WIDTH + range;
-            draw_to_canvas(
-                &context,
+            arcs.push(calculate_face(
+                // &context,
                 i as f64,
                 x_offset as f64,
                 y_offset as f64,
                 skew_x as f64,
                 skew_y as f64,
-            );
+            ));
         }
     }
+
+    Faces { arcs }
 }
 
-struct Arc {
-    x: f64,
-    y: f64,
-    z: f64,
-    t: f64,
-    rad: f64,
-}
+struct Arc(f64, f64, f64, f64, f64);
 
 struct Face {
     mouth: Arc,
@@ -134,86 +131,119 @@ struct Faces {
     arcs: Vec<Face>,
 }
 
-
-fn calculate_faces(
-    context: &Context2d,
+fn calculate_face(
+    // context: &Context2d,
     iter: f64,
     x_offset: f64,
     y_offset: f64,
     x_skew: f64,
     y_skew: f64,
-) {
+) -> Face {
     let offset_x = x_offset + (iter % 4.0) + (x_skew / 100.0 * iter);
     let offset_y = y_offset + (iter % 4.0) + (y_skew / 100.0 * iter);
 
-    let mouth = { x:  75.0 + offset_x, y: 75.0 + offset_y, z: 35.0 + iter, 0.0, f64::consts::Pi };
+    let mouth = Arc(
+        75.0 + offset_x,
+        75.0 + offset_y,
+        35.0 + iter,
+        0.0,
+        f64::consts::PI,
+    );
+    let left_eye = Arc(
+        60.0 + offset_x,
+        65.0 + offset_y,
+        5.0 + iter,
+        0.0,
+        f64::consts::PI * 2.0,
+    );
+    let right_eye = Arc(
+        90.0 + offset_x,
+        65.0 + offset_y,
+        5.0 + iter,
+        0.0,
+        f64::consts::PI * 2.0,
+    );
+    let outer_circle = Arc(
+        75.0 + offset_x,
+        75.0 + offset_y,
+        50.0 + iter,
+        0.0,
+        f64::consts::PI * 2.0,
+    );
+
+    Face {
+        mouth,
+        left_eye,
+        right_eye,
+        outer_circle,
+    }
 }
 
-fn draw_to_canvas(
-    context: &Context2d,
-    iter: f64,
-    x_offset: f64,
-    y_offset: f64,
-    x_skew: f64,
-    y_skew: f64,
-) {
-    context.set_stroke_style(&JsValue::from_str("#f00"));
-    context.begin_path();
-    let offset_x = x_offset + (iter % 4.0) + (x_skew / 100.0 * iter);
-    let offset_y = y_offset + (iter % 4.0) + (y_skew / 100.0 * iter);
+// fn draw_to_canvas(
+//     context: &Context2d,
+//     iter: f64,
+//     x_offset: f64,
+//     y_offset: f64,
+//     x_skew: f64,
+//     y_skew: f64,
+// ) {
+//     context.set_stroke_style(&JsValue::from_str("#f00"));
+//     context.begin_path();
+//     let offset_x = x_offset + (iter % 4.0) + (x_skew / 100.0 * iter);
+//     let offset_y = y_offset + (iter % 4.0) + (y_skew / 100.0 * iter);
 
-    // Draw the outer circle.
-    context
-        .arc(
-            75.0 + offset_x,
-            75.0 + offset_y,
-            50.0 + iter,
-            0.0,
-            f64::consts::PI * 2.0,
-        )
-        .unwrap();
-    context.stroke();
+//     // Draw the outer circle.
+//     context
+//         .arc(
+//             75.0 + offset_x,
+//             75.0 + offset_y,
+//             50.0 + iter,
+//             0.0,
+//             f64::consts::PI * 2.0,
+//         )
+//         .unwrap();
+//     context.stroke();
 
-    // Draw the mouth.
-    context.set_stroke_style(&JsValue::from_str("blue"));
-    context.begin_path();
-    context
-        .arc(
-            75.0 + offset_x,
-            75.0 + offset_y,
-            35.0 + iter,
-            0.0,
-            f64::consts::PI,
-        )
-        .unwrap();
-    context.stroke();
+//     // Draw the mouth.
+//     context.set_stroke_style(&JsValue::from_str("blue"));
+//     context.begin_path();
+//     context
+//         .arc(
+//             75.0 + offset_x,
+//             75.0 + offset_y,
+//             35.0 + iter,
+//             0.0,
+//             f64::consts::PI,
+//         )
+//         .unwrap();
+//     context.stroke();
 
-    // // Draw the left eye.
-    context.set_stroke_style(&JsValue::from_str("purple"));
-    context.begin_path();
-    context
-        .arc(
-            60.0 + offset_x,
-            65.0 + offset_y,
-            5.0 + iter,
-            0.0,
-            f64::consts::PI * 2.0,
-        )
-        .unwrap();
-    context.stroke();
+//     // // Draw the left eye.
+//     context.set_stroke_style(&JsValue::from_str("purple"));
+//     context.begin_path();
+//     context
+//         .arc(
+//             60.0 + offset_x,
+//             65.0 + offset_y,
+//             5.0 + iter,
+//             0.0,
+//             f64::consts::PI * 2.0,
+//         )
+//         .unwrap();
+//     context.stroke();
 
-    // Draw the right eye.
-    context.set_stroke_style(&JsValue::from_str("green"));
-    context.begin_path();
-    context
-        .arc(
-            90.0 + offset_x,
-            65.0 + offset_y,
-            5.0 + iter,
-            0.0,
-            f64::consts::PI * 2.0,
-        )
-        .unwrap();
+//     // Draw the right eye.
+//     context.set_stroke_style(&JsValue::from_str("green"));
+//     context.begin_path();
+//     context
+//         .arc(
+//             90.0 + offset_x,
+//             65.0 + offset_y,
+//             5.0 + iter,
+//             0.0,
+//             f64::consts::PI * 2.0,
+//         )
+//         .unwrap();
 
-    context.stroke();
-}
+//     context.stroke();
+// }
